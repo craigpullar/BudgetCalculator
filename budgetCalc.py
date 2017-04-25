1fb57067f31a6d8412212ee7fce4acd4b7396375
craigpullar <craigmichaelpullar@gmail.com>
2017-04-25 16:10:03 +0100

    Added dates

--- budgetCalc.py
+++ budgetCalc.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 import json
+from datetime import datetime
 from pprint import pprint
 
 def calculate(current, desired, budget): 
+	today = datetime.now()
+	today = today.day
 	with open('budget.json') as data_file:
 		data = json.load(data_file)
 	for key in data:
-		current -= data[key]
+		if data[key]['date'] < 27 and today <= data[key]['date']:
+			current -= data[key]['value']
+		elif today >= 27 and today <= data[key]['date'] and data[key]['date'] >= 27:
+			current -= data[key]['value']
 	diff = current - desired
 	print "You have: £%s left to spend this month" % diff
 
