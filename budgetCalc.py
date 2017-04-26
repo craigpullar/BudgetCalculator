660a7669119b413a68ce1ed4769ab3e555e89e63
craigpullar <craigmichaelpullar@gmail.com>
2017-04-26 17:14:26 +0100

    Added recurring payments functionality

--- budgetCalc.py
+++ budgetCalc.py
@@ -6,19 +6,27 @@ from pprint import pprint
 def calculate(current, desired, budget): 
 	today = datetime.now()
 	today = today.day
-	today = 1
 	if today > 27:
 		today -= 27
 	with open('budget.json') as data_file:
 		data = json.load(data_file)
-	for key in data:
+	for key in list(data):
+		if data[key]['rec'] > 1:
+			i = 0
+			while i < data[key]['rec']:
+				new_data = data[key]
+				new_data['date'] += 7
+				new_key = key + str(i)
+				data[new_key] = new_data
+				i += 1
 		if data[key]['date'] > 27:
 			data[key]['date'] -= 27
 		else:
 			data[key]['date'] += 3
 		if today <= data[key]['date']:
 			current -= data[key]['value']
+
 	diff = current - desired
 	print "You have: £%s left to spend this month" % diff
 
-calculate(656,400,0)
+calculate(297,200,0)
