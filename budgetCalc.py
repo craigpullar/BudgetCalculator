37051e160b6942150da4348a80b5becfcdc3be8a
craigpullar <craigmichaelpullar@gmail.com>
2017-07-03 11:11:32 +0100

    Small fixes to accuracy of dates

--- budgetCalc.py
+++ budgetCalc.py
@@ -5,32 +5,36 @@ from pprint import pprint
 
 def calculate(current, desired, budget): 
 	today = datetime.now()
-	today = today.day + 1
+	today = today.day
 	if today > 27:
 		today -= 27
+	else:
+		today += 3
 	with open('budget.json') as data_file:
 		data = json.load(data_file)
+	
 	for key in list(data):
-		if data[key]['rec'] > 1:
-			i = 1
-			while i < data[key]['rec']:
-
-				new_data = data[key]
-				new_data['date'] += 7
-				new_key = key + str(i)
-				data[new_key] = {'date' : new_data['date'], 'rec' : 1, 'value' : new_data['value']}
-				i += 1
-	for key in list(data):
-		print key
-		print data[key]
 		if data[key]['date'] > 27:
 			data[key]['date'] -= 27
 		else:
 			data[key]['date'] += 3
+	for key in list(data):
+		if data[key]['rec'] > 1:
+			print data[key]
+			i = 1
+			while i < data[key]['rec']:
+				new_key = key + str(i)
+				new_date = data[key]['date'] + (7*i)
+				data[new_key] = {'date' : new_date, 'rec' : 1, 'value' : data[key]['value']}
+				i += 1
+	for key in list(data):
+		if data[key]['date'] > today:
+			print key
+			print data[key]
+	for key in list(data):
 		if today <= data[key]['date']:
 			current -= data[key]['value']
-
-	diff = current - desired
+			diff = current - desired
 	print "You have: £%s left to spend this month" % diff
 
-calculate(1270,0,0)
+calculate(929,0,0)
